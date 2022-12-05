666f20ed87521dd1c55b01cc203337cc256e89d9
Luis Carlos <lucaldev22@gmail.com>
2022-12-05 15:56:51 -0300

    Fixing errors.

--- src/components/Header/header.test.tsx
+++ src/components/Header/header.test.tsx
@@ -4,7 +4,7 @@ import Header from './index';
 
 describe('<Header />', () => {
   it('should render', () => {
-    const header = renderTheme(<Header />);
+    const header = renderTheme(<Header number={0} />);
     expect(header).toMatchSnapshot;
   });
 });
